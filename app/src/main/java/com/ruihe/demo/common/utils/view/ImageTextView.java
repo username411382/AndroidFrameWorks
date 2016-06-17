614693848c7b75a623fcba01a15e049bf0fab33f
ruihe <411382221@qq.com>
2016-06-17 17:18:24 +0800

    [TASK]联系人按字母索引漏掉的文件

---
+++ app/src/main/java/com/ruihe/demo/common/utils/view/ImageTextView.java
@@ -0,0 +1,41 @@
+package com.ruihe.demo.common.utils.view;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.ruihe.demo.common.utils.BitmapUtil;
+
+/**
+ * 文字图片，这个相信大家都知道，比如QQ底部导航上的未读消息数
+ */
+public class ImageTextView extends TextView {
+    private Bitmap bitmap;
+    private String text;
+    Drawable d;
+
+    public ImageTextView(Context context) {
+        super(context);
+
+    }
+
+    public ImageTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+    }
+
+    public ImageTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+    public void setIconText(Context context, String text) {
+        text = this.getText().toString().substring(0, 1);
+        bitmap = BitmapUtil.getIndustry(context, text);
+        d = BitmapUtil.bitmapTodrawable(bitmap);
+        this.setCompoundDrawables(d, null, null, null);
+    }
+
+}
